7b5708f0ce60a3e6128b407c7578b4f8d8145299
Ryan Marcotte <RyanM@iqmetrix.com>
2017-01-20 21:57:15 -0600

    added awesome spaceship graphic; bounding box rotation now fixed; need to rotate sprite

--- Subwoofer/Assets/Scripts/PlayerController.cs
+++ Subwoofer/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	private const int THRUST_SPEED = 15;
-	private const int ROTATION_SPEED = 500;
+	private const int ROTATION_SPEED = 1;
 
 	//Use of rigid body allows the physics engine to apply
 	private Rigidbody2D _rb;
-    
+	private float _rotation;
 
     //The start function runs once at the beginning of the game
     void Start ()
@@ -25,17 +25,11 @@ public class PlayerController : MonoBehaviour
         //Obtain the movements
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
-
-		/*if (inputY > 0 || inputY < 0)
-			_rb.velocity = new Vector2 (0.0f, 0.0f);*/
-
-		if (inputX == 0)
-			_rb.angularVelocity = 0.0f;
 		
         //Apply the movements
-		Vector2 movement = new Vector2(inputY * (float)Math.Sin(_rb.rotation * (Math.PI / 180.0f)), inputY * (float)Math.Cos(_rb.rotation * (Math.PI / 180.0f)));
+		var movement = new Vector2(inputY * (float)Math.Sin(_rotation * (Math.PI / 180.0f)), inputY * (float)Math.Cos(_rotation * (Math.PI / 180.0f)));
 
 		_rb.AddForce(movement * THRUST_SPEED);
-		_rb.AddTorque(inputX * ROTATION_SPEED);
+		_rotation += inputX * ROTATION_SPEED;
     }
 }
