31ecf79d81ed2270c28cf49e72c8a189eb95b9d6
Ryan Marcotte <RyanM@iqmetrix.com>
2017-01-20 21:26:42 -0600

    mostly good on ship movement physics

--- Subwoofer/Assets/Scripts/PlayerController.cs
+++ Subwoofer/Assets/Scripts/PlayerController.cs
@@ -1,35 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
     //Use of rigid body allows the physics engine to apply
-    private Rigidbody rb;
-    private int speed;
+    private Rigidbody2D rb;
+    private const int THRUST_SPEED = 80;
+	private const int ROTATION_SPEED = 500;
 
     //The start function runs once at the beginning of the game
     void Start ()
     {
         //Obtain a reference to the rigid body
-        rb = GetComponent<Rigidbody>();
-
-        speed = 100;
+        rb = GetComponent<Rigidbody2D>();
     }
 	
 	//The update function runs each frame
 	void FixedUpdate ()
     {
         //Obtain the movements
-        //float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
 
-		if (moveVertical > 0)
-			rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+		if (inputY > 0 || inputY < 0) {
+			rb.velocity = new Vector2 (0.0f, 0.0f);
+		}
+
+		if (inputX == 0)
+			rb.angularVelocity = 0.0f;
 		
         //Apply the movements
-        Vector2 movement = new Vector2(0.0f, moveVertical);
+		Vector2 movement = new Vector2(inputY * (float)Math.Sin(rb.rotation * (Math.PI / 180.0f)), inputY * (float)Math.Cos(rb.rotation * (Math.PI / 180.0f)));
 
-        rb.AddForce(movement * speed);
+		rb.AddForce(movement * THRUST_SPEED);
+		rb.AddTorque(inputX * ROTATION_SPEED);
     }
 }
