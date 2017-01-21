31a84f181e06f3cf61df461ddd7c9afc37a17385
Tristan Heisler <tristanheisler@hotmail.ca>
2017-01-20 20:21:36 -0600

    Basic movement.

--- Subwoofer/Assets/Scripts/PlayerController.cs
+++ Subwoofer/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,19 @@ public class PlayerController : MonoBehaviour {
 
     //Use of rigid body allows the physics engine to apply
     private Rigidbody rb;
+    private int speed;
 
     //The start function runs once at the beginning of the game
     void Start ()
     {
         //Obtain a reference to the rigid body
         rb = GetComponent<Rigidbody>();
+
+        speed = 10;
     }
 	
 	//The update function runs each frame
-	void Update ()
+	void FixedUpdate ()
     {
         //Obtain the movements
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -23,6 +26,6 @@ public class PlayerController : MonoBehaviour {
 
         //Apply the movements
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb.AddForce(movement);
+        rb.AddForce(movement * speed);
     }
 }
