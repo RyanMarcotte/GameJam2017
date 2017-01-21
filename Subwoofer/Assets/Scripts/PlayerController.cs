2c2c580c41a7e89fdddd6ce4c86b3a7719d776de
Ryan Marcotte <RyanM@iqmetrix.com>
2017-01-20 21:39:02 -0600

    minor adjustments

--- Subwoofer/Assets/Scripts/PlayerController.cs
+++ Subwoofer/Assets/Scripts/PlayerController.cs
@@ -3,18 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    //Use of rigid body allows the physics engine to apply
-    private Rigidbody2D rb;
-    private const int THRUST_SPEED = 80;
+	private const int THRUST_SPEED = 20;
 	private const int ROTATION_SPEED = 500;
+
+	//Use of rigid body allows the physics engine to apply
+	private Rigidbody2D _rb;
+    
 
     //The start function runs once at the beginning of the game
     void Start ()
     {
         //Obtain a reference to the rigid body
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 	
 	//The update function runs each frame
@@ -24,17 +27,16 @@ public class PlayerController : MonoBehaviour {
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
 
-		if (inputY > 0 || inputY < 0) {
-			rb.velocity = new Vector2 (0.0f, 0.0f);
-		}
+		/*if (inputY > 0 || inputY < 0)
+			_rb.velocity = new Vector2 (0.0f, 0.0f);*/
 
 		if (inputX == 0)
-			rb.angularVelocity = 0.0f;
+			_rb.angularVelocity = 0.0f;
 		
         //Apply the movements
-		Vector2 movement = new Vector2(inputY * (float)Math.Sin(rb.rotation * (Math.PI / 180.0f)), inputY * (float)Math.Cos(rb.rotation * (Math.PI / 180.0f)));
+		Vector2 movement = new Vector2(inputY * -(float)Math.Sin(_rb.rotation * (Math.PI / 180.0f)), inputY * (float)Math.Cos(_rb.rotation * (Math.PI / 180.0f)));
 
-		rb.AddForce(movement * THRUST_SPEED);
-		rb.AddTorque(inputX * ROTATION_SPEED);
+		_rb.AddForce(movement * THRUST_SPEED);
+		_rb.AddTorque(inputX * ROTATION_SPEED);
     }
 }
