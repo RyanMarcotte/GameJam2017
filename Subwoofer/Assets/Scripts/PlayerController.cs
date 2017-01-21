2f1ef93d37b18ec8aac98cdbf157e1db8d180504
Tristan Heisler <tristanheisler@hotmail.ca>
2017-01-20 20:15:18 -0600

    Added basis for player and background.

---
+++ Subwoofer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //Use of rigid body allows the physics engine to apply
+    private Rigidbody rb;
+
+    //The start function runs once at the beginning of the game
+    void Start ()
+    {
+        //Obtain a reference to the rigid body
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	//The update function runs each frame
+	void Update ()
+    {
+        //Obtain the movements
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Apply the movements
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb.AddForce(movement);
+    }
+}
